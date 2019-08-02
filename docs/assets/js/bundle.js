c5f72ef95d8484e327131748b64c312b60124793
iq3 <iq3@addli.co.jp>
2019-08-02 12:48:57 +0900

    Added expired check for JWT.

--- docs/assets/js/bundle.js
+++ docs/assets/js/bundle.js
@@ -4176,6 +4176,101 @@
       }
   }
 
+  /**
+   * The code was extracted from:
+   * https://github.com/davidchambers/Base64.js
+   */
+
+  var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+  function InvalidCharacterError(message) {
+    this.message = message;
+  }
+
+  InvalidCharacterError.prototype = new Error();
+  InvalidCharacterError.prototype.name = 'InvalidCharacterError';
+
+  function polyfill (input) {
+    var str = String(input).replace(/=+$/, '');
+    if (str.length % 4 == 1) {
+      throw new InvalidCharacterError("'atob' failed: The string to be decoded is not correctly encoded.");
+    }
+    for (
+      // initialize result and counters
+      var bc = 0, bs, buffer, idx = 0, output = '';
+      // get next character
+      buffer = str.charAt(idx++);
+      // character found in table? initialize bit storage and add its ascii value;
+      ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer,
+        // and if not first of each 4 characters,
+        // convert the first 8 bits to one ascii character
+        bc++ % 4) ? output += String.fromCharCode(255 & bs >> (-2 * bc & 6)) : 0
+    ) {
+      // try to find character in table (0-63, not found => -1)
+      buffer = chars.indexOf(buffer);
+    }
+    return output;
+  }
+
+
+  var atob = typeof window !== 'undefined' && window.atob && window.atob.bind(window) || polyfill;
+
+  function b64DecodeUnicode(str) {
+    return decodeURIComponent(atob(str).replace(/(.)/g, function (m, p) {
+      var code = p.charCodeAt(0).toString(16).toUpperCase();
+      if (code.length < 2) {
+        code = '0' + code;
+      }
+      return '%' + code;
+    }));
+  }
+
+  var base64_url_decode = function(str) {
+    var output = str.replace(/-/g, "+").replace(/_/g, "/");
+    switch (output.length % 4) {
+      case 0:
+        break;
+      case 2:
+        output += "==";
+        break;
+      case 3:
+        output += "=";
+        break;
+      default:
+        throw "Illegal base64url string!";
+    }
+
+    try{
+      return b64DecodeUnicode(output);
+    } catch (err) {
+      return atob(output);
+    }
+  };
+
+  function InvalidTokenError(message) {
+    this.message = message;
+  }
+
+  InvalidTokenError.prototype = new Error();
+  InvalidTokenError.prototype.name = 'InvalidTokenError';
+
+  var lib = function (token,options) {
+    if (typeof token !== 'string') {
+      throw new InvalidTokenError('Invalid token specified');
+    }
+
+    options = options || {};
+    var pos = options.header === true ? 0 : 1;
+    try {
+      return JSON.parse(base64_url_decode(token.split('.')[pos]));
+    } catch (e) {
+      throw new InvalidTokenError('Invalid token specified: ' + e.message);
+    }
+  };
+
+  var InvalidTokenError_1 = InvalidTokenError;
+  lib.InvalidTokenError = InvalidTokenError_1;
+
   class UserLocalStorageRepository {
       constructor() {
           this.user = () => {
@@ -4183,8 +4278,16 @@
               if (value == null) {
                   return null;
               }
-              const object = JSON.parse(value);
-              return User.init(object);
+              const user = User.init(JSON.parse(value));
+              // check expired
+              let decoded = lib(user.token);
+              let now = Date.now() / 1000; // mili sec
+              let exp = Number(decoded["exp"]); // sec
+              if (now > exp) {
+                  this.setUser(null);
+                  return null;
+              }
+              return user;
           };
           this.setUser = (user) => {
               if (user) {
--- src/Domain/Repository/UserLocalStorageRepository.ts
+++ src/Domain/Repository/UserLocalStorageRepository.ts
@@ -1,13 +1,24 @@
 import UserRepository from "./interface/UserRepository"
 import User from "../Model/User"
+import jwt_decode from "jwt-decode"
 
 export default class UserLocalStorageRepository implements UserRepository {
 
     user = () => {
         let value = localStorage.getItem("user")
         if ( value == null ) { return null }
-        const object = JSON.parse( value )
-        return User.init(object)
+        const user = User.init(JSON.parse( value ))
+
+        // check expired
+        let decoded = jwt_decode( user.token )
+        let now = Date.now() / 1000 // mili sec
+        let exp = Number(decoded["exp"]) // sec
+        if ( now > exp ) {
+            this.setUser(null)
+            return null
+        }
+
+        return user
     }
 
     setUser = (user: User) => {
@@ -22,6 +33,6 @@ export default class UserLocalStorageRepository implements UserRepository {
     }
 
     isLoggedIn = () => {
-        return this.user() != null // todo: need check for expire
+        return this.user() != null
     }
 }
