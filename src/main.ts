60f48848e910a603b16089b1fe1b9edbed64712c
iq3 <iq3@addli.co.jp>
2019-08-12 17:55:42 +0900

    Removed unuse code.

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,10 @@
 import "fetch-polyfill"
 
 // Import Framework
-import { component, install } from "riot"
+import { component } from "riot"
 
 // Import application with riot
 import application from "./Presentation/application.riot"
-
-// Install plugin
-// install((component) => {
-//     const { onBeforeMount } = component
-//     component.onBeforeMount = (props, state) => {
-//         if (props["ref"]) {
-//             props["ref"](component)
-//         }
-//         onBeforeMount.apply(component, [props, state])
-//     }
-//     return component
-// })
 
 // Start Application
 component(application)( document.getElementById("application") )
